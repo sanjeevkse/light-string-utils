641010baaa07a5ce2526e1f82becc9a5a8ff06df
Sanjeev Shetty <sanjeevkse@gmail.com>
2023-04-28 11:46:37 +0530

    index issue solved

---
+++ index.js
@@ -0,0 +1,43 @@
+var abbreviate = require("./src/abbreviate");
+var capitalize = require("./src/capitalize");
+var center = require("./src/center");
+var deCapitalize = require("./src/deCapitalize");
+var endsWith = require("./src/endsWith");
+var equalsIgnoreCase = require("./src/equalsIgnoreCase");
+var escapeHTML = require("./src/escapeHTML");
+var first = require("./src/first");
+var isAlNum = require("./src/isAlNum");
+var isAlpha = require("./src/isAlpha");
+var last = require("./src/last");
+var length = require("./src/length");
+var limit = require("./src/limit");
+var pad = require("./src/pad");
+var replaceBy = require("./src/replaceBy");
+var sentenceCase = require("./src/sentenceCase");
+var slugify = require("./src/slugify");
+var subString = require("./src/subString");
+var titleCase = require("./src/titleCase");
+var toArray = require("./src/toArray");
+
+module.exports = {
+  abbreviate,
+  capitalize,
+  center,
+  deCapitalize,
+  endsWith,
+  equalsIgnoreCase,
+  escapeHTML,
+  first,
+  isAlNum,
+  isAlpha,
+  last,
+  length,
+  limit,
+  pad,
+  replaceBy,
+  sentenceCase,
+  slugify,
+  subString,
+  titleCase,
+  toArray,
+};
